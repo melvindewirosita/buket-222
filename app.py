be74c1e063436c5be2a6660601b93724350f6119
melvindewirosita <dewiiroosita12@gmail.com>
2024-04-02 05:01:44 +0700

    First Commit

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get('mongodb+srv://dewiiroosita12:melvindr@cluster0.ek1gznh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+DB_NAME =  os.environ.get('melvindr')
+
+client = MongoClient('mongodb+srv://dewiiroosita12:melvindr@cluster0.ek1gznh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+db = client.dbsparta
 
 app = Flask(__name__)
 
